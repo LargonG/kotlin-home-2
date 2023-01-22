0fa34cb70a9b3e3d13dd45d849e143b9e13bcf2b
LargonG <pan300503@gmail.com>
2023-01-22 15:55:56 +0300

    Added solution for heap & tree & flist

--- lib/src/main/kotlin/binomial/BinomialTree.kt
+++ lib/src/main/kotlin/binomial/BinomialTree.kt
@@ -1,5 +1,7 @@
 package binomial
 
+import java.lang.IllegalArgumentException
+
 interface SelfMergeable<T> {
     operator fun plus(other: T): T
 }
@@ -16,15 +18,16 @@ interface SelfMergeable<T> {
  *  - циклы
  *  - стандартные коллекции
  *
- * Детали внутренней реазации должны быть спрятаны
+ * Детали внутренней реазации должны быть спрятаны - (А они нужны?)
  * Создание - только через single() и plus()
  *
  * Дерево совсем без элементов не предусмотрено
  */
 
-class BinomialTree<T: Comparable<T>> private constructor(val value: T, val children: FList<BinomialTree<T>>): SelfMergeable<BinomialTree<T>> {
+class BinomialTree<T: Comparable<T>>
+    private constructor(val value: T, val children: FList<BinomialTree<T>>): SelfMergeable<BinomialTree<T>> {
     // порядок дерева
-    val order: Int = TODO()
+    val order: Int = children.size
 
     /*
      * слияние деревьев
@@ -33,10 +36,18 @@ class BinomialTree<T: Comparable<T>> private constructor(val value: T, val child
      * Требуемая сложность - O(1)
      */
     override fun plus(other: BinomialTree<T>): BinomialTree<T> {
-        TODO()
+        if (order != other.order)
+            throw IllegalArgumentException("Different orders of trees");
+
+        // Поддерживаем инвариант, что дети отсортированы в порядке возрастания order
+        return if (value < other.value) {
+            BinomialTree(value, FList.Cons(other, children.reverse()).reverse())
+        } else {
+            BinomialTree(other.value, FList.Cons(this, other.children.reverse()).reverse())
+        }
     }
 
     companion object {
-        fun <T: Comparable<T>> single(value: T): BinomialTree<T> = TODO()
+        fun <T: Comparable<T>> single(value: T): BinomialTree<T> = BinomialTree(value, flistOf())
     }
 }
--- lib/src/main/kotlin/binomial/FList.kt
+++ lib/src/main/kotlin/binomial/FList.kt
@@ -1,5 +1,7 @@
 package binomial
 
+import java.util.NoSuchElementException
+
 /*
  * FList - реализация функционального списка
  *
@@ -43,6 +45,12 @@ sealed class FList<T>: Iterable<T> {
         Cons(current, acc)
     }
 
+    // Решил реализовать собственный метод,
+    // чтобы каждый раз не писать что-то в духе: iterator().next()
+    // Берёт первый элемент
+    // O(1)
+    abstract fun first(): T
+
     /*
      * Это не очень красиво, что мы заводим отдельный Nil на каждый тип
      * И вообще лучше, чтобы Nil был объектом
@@ -56,9 +64,31 @@ sealed class FList<T>: Iterable<T> {
      * Также для борьбы с бойлерплейтом были введены функция и свойство nil в компаньоне
      */
     data class Nil<T>(private val dummy: Int=0) : FList<T>() {
+        override val size = 0
+        override val isEmpty = true
+        override fun <U> fold(base: U, f: (U, T) -> U): U = base
+
+        override fun filter(f: (T) -> Boolean): FList<T> = this
+
+        override fun <U> map(f: (T) -> U): FList<U> = Nil()
+
+        override fun iterator(): Iterator<T> = FListIterator(this)
+
+        override fun first(): T = throw NoSuchElementException("It's empty list")
     }
 
     data class Cons<T>(val head: T, val tail: FList<T>) : FList<T>() {
+        override val size = 1 + tail.size;
+        override val isEmpty = false
+        override fun <U> fold(base: U, f: (U, T) -> U): U = tail.fold(f(base, head), f)
+
+        override fun filter(f: (T) -> Boolean): FList<T> = if (f(head)) Cons(head, tail.filter(f)) else tail.filter(f)
+
+        override fun <U> map(f: (T) -> U): FList<U> = Cons(f(head), tail.map(f));
+
+        override fun iterator(): Iterator<T> = FListIterator(this)
+
+        override fun first(): T = head
     }
 
     companion object {
@@ -67,8 +97,34 @@ sealed class FList<T>: Iterable<T> {
     }
 }
 
+class FListIterator<T>(private var node: FList<T>): Iterator<T> {
+
+    override fun hasNext(): Boolean = !node.isEmpty
+
+    override fun next(): T = when(node) {
+        is FList.Nil<T> -> throw NoSuchElementException("Next element does not exist")
+        is FList.Cons<T> -> {
+            // Mutable - классная вещь, ничего не скажешь
+            // Вот эти вот страшная конструкции из-за того, что компилятор думает,
+            // что эта переменная может меняться в других потоках,
+            // но мы ведь делаем без многопоточки
+            val cnode = node as FList.Cons<T>
+            val res = cnode.head
+            node = cnode.tail
+            res
+        }
+    }
+}
+
+
 // конструирование функционального списка в порядке следования элементов
 // требуемая сложность - O(n)
-fun <T> flistOf(vararg values: T): FList<T> {
-    TODO()
+fun <T> flistOf(vararg values: T): FList<T> = flistOf<T>(values, 0)
+
+private fun <T> flistOf(values: Array<out T>, index: Int): FList<T> {
+    return if (index >= values.size) {
+        FList.Nil()
+    } else {
+        FList.Cons(values[index], flistOf(values, index + 1))
+    }
 }
