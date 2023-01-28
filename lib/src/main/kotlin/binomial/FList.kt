0aa5e47446dec564faa4262635e3f544c04b1f9f
LargonG <pan300503@gmail.com>
2023-01-28 12:53:06 +0300

    Fixed stack overflow solution
    * Добавлены приватные методы, работающие на итераторах. При этом они используют хвостовую рекурсию
    * Исправлен приватный глобальный метод flistOf, теперь он тоже использует хвостовую рекурсию

--- lib/src/main/kotlin/binomial/FList.kt
+++ lib/src/main/kotlin/binomial/FList.kt
@@ -80,15 +80,45 @@ sealed class FList<T>: Iterable<T> {
     data class Cons<T>(val head: T, val tail: FList<T>) : FList<T>() {
         override val size = 1 + tail.size;
         override val isEmpty = false
-        override fun <U> fold(base: U, f: (U, T) -> U): U = tail.fold(f(base, head), f)
+        override fun <U> fold(base: U, f: (U, T) -> U): U = foldRec(base, f, iterator())
 
-        override fun filter(f: (T) -> Boolean): FList<T> = if (f(head)) Cons(head, tail.filter(f)) else tail.filter(f)
+        override fun filter(f: (T) -> Boolean): FList<T> = filterRec(Nil(), f, iterator()).reverse()
 
-        override fun <U> map(f: (T) -> U): FList<U> = Cons(f(head), tail.map(f));
+        override fun <U> map(f: (T) -> U): FList<U> = mapRec(Nil(), f, iterator()).reverse()
 
         override fun iterator(): Iterator<T> = FListIterator(this)
 
         override fun first(): T = head
+
+        companion object {
+            // Функции выглядят похоже, но вроде их нельзя эффективно объединить,
+            // потому что типы разные и принимаемые параметры тоже разные
+            private tailrec fun <U, T> foldRec(base: U, f: (U, T) -> U, iterator: Iterator<T>): U {
+                if (iterator.hasNext()) {
+                    val next = iterator.next();
+                    return foldRec(f(base, next), f, iterator)
+                }
+                return base;
+            }
+
+            // Возвращает перевёрнутый отфильтрованный список
+            private tailrec fun <T> filterRec(tail: FList<T>, f: (T) -> Boolean, iterator: Iterator<T>): FList<T> {
+                if (iterator.hasNext()) {
+                    val next = iterator.next();
+                    return if (f(next)) filterRec(Cons(next, tail), f, iterator) else filterRec(tail, f, iterator)
+                }
+                return tail;
+            }
+
+            // Возвращает перевёрнутый отформатированный список
+            private tailrec fun <U, T> mapRec(tail: FList<U>, f: (T) -> U, iterator: Iterator<T>): FList<U> {
+                if (iterator.hasNext()) {
+                    val next = iterator.next();
+                    return mapRec(Cons(f(next), tail), f, iterator)
+                }
+                return tail;
+            }
+        }
     }
 
     companion object {
@@ -119,12 +149,12 @@ class FListIterator<T>(private var node: FList<T>): Iterator<T> {
 
 // конструирование функционального списка в порядке следования элементов
 // требуемая сложность - O(n)
-fun <T> flistOf(vararg values: T): FList<T> = flistOf<T>(values, 0)
+fun <T> flistOf(vararg values: T): FList<T> = flistOf(FList.Nil(), values.iterator()).reverse()
 
-private fun <T> flistOf(values: Array<out T>, index: Int): FList<T> {
-    return if (index >= values.size) {
-        FList.Nil()
-    } else {
-        FList.Cons(values[index], flistOf(values, index + 1))
+private tailrec fun <T> flistOf(tail: FList<T>, iterator: Iterator<T>): FList<T> {
+    if (iterator.hasNext()) {
+        val head = iterator.next();
+        return flistOf(FList.Cons(head, tail), iterator)
     }
+    return tail
 }
